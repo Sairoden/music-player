e01d739f61c1d1bb2f3fcc0120d41dd4d80e7cfd
Sairoden <odingandarosa@gmail.com>
2021-09-18 14:21:34 +0800

    Update Input Slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -20,10 +20,9 @@ const Player = ({ currentSong, setIsPlaying, isPlaying }) => {
   };
 
   const timeUpdateHandler = e => {
-    const current = e.target.currentTime;
+    const currentTime = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
-    console.log(songInfo.currentTime);
+    setSongInfo({ ...songInfo, currentTime, duration });
   };
 
   const getTime = time => {
@@ -32,17 +31,31 @@ const Player = ({ currentSong, setIsPlaying, isPlaying }) => {
     );
   };
 
+  const dragHandler = e => {
+    const currentTime = e.target.value;
+
+    audioRef.current.currentTime = currentTime;
+
+    setSongInfo({ ...songInfo, currentTime });
+  };
+
   // State
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: 0,
+    duration: 0,
   });
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          type="range"
+          onChange={dragHandler}
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
 
